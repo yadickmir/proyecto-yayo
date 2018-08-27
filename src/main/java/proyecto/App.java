7b553ac1f888592576e57b27838fb593e3503cb2
Yadickmir Soto <yadickmir@gmail.com>
2018-08-26 22:42:52 -0300

    Agrega aplicacion base

---
+++ src/main/java/proyecto/App.java
@@ -0,0 +1,13 @@
+package proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
